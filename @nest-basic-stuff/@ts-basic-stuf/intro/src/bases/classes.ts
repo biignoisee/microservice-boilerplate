83692eafaef024f185b0ca80572c2eeeee3db745
Omar Gabriel <omareegab@gmail.com>
2026-09-05 14:14:30 -0500

    classes, axios y async

--- @nest-basic-stuff/@ts-basic-stuf/intro/src/bases/classes.ts
+++ @nest-basic-stuff/@ts-basic-stuf/intro/src/bases/classes.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 // representacion de un objeto de la vida real, con propiedades y metodos
 export class Artist {
   public id: number;
@@ -21,13 +23,46 @@ export class Artist {
 
 //another form
 export class Producers {
+
+  get imageUrl(): string {
+    return `https://example.com/${this.id}.jpg`;
+  }
+
   constructor(
     public readonly id: number,
-    public name: string
+    public name: string,
+    // public imageUrl?: string
   ) { }
+
+  // ----------------------
+  // Métodos
+  // ----------------------
+
+  public scream() {
+    console.log(`Hi, I'm ${this.name.toUpperCase()}!!!!`);
+  }
+
+  //public
+  speak() {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+
+  // Async
+  async getMoves() {
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.id}`);
+    const moves = data.moves;
+    return moves;
+  }
 }
 
-
+// public se puede acceder desde cualquier lugar
+// private solo se puede acceder desde la clase
+// protected se puede acceder desde la clase y sus subclases
 
 export const lilWayne = new Artist(1, "Lil Wayne");
-export const drDre = new Producers(1, "Dr. Dre");
+export const drDre = new Producers(11, "Dr. Dre");
+
+drDre.speak(); // Hi, I'm Dr. Dre
+drDre.scream(); // Hi, I'm DR. DRE!!!!
+
+console.log(drDre.getMoves().then(moves => console.log(moves)))
