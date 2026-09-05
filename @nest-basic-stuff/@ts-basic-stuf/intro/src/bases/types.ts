68a2982d4801767b191673eab3095495e74d37a4
Omar Gabriel <omareegab@gmail.com>
2026-09-05 11:25:53 -0500

    some basic stuff

---
+++ @nest-basic-stuff/@ts-basic-stuf/intro/src/bases/types.ts
@@ -0,0 +1,7 @@
+export const name = 'Omar';
+export const age: number = 24;
+
+export const isValid: boolean = true;
+
+
+//esto es un modulo, al momento de exportar, siempre se va a ejecutar dentro de aca, no coloque ejecutables a menos que quiera
