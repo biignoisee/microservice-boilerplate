68d198a05f625c80027c6b938468419c739ef27c
Omar Gabriel <omareegab@gmail.com>
2026-09-07 14:54:09 -0500

    liskov principle

--- @nest-basic-stuff/@ts-basic-stuf/intro/src/api/pokeApi.adapter.ts
+++ @nest-basic-stuff/@ts-basic-stuf/intro/src/api/pokeApi.adapter.ts
@@ -2,12 +2,20 @@
 
 import axios from "axios";
 
+export class PokeApiFetchAdapter {
+  async get<T>(url: string): Promise<T> {
+    const response = await fetch(url);
+    const data: T = await response.json();
+    return data;
+  }
+}
 
 export class PokeApiAdapter {
   private readonly axios = axios;
 
-  async get(url: string) {
-    const { data } = await this.axios.get(url);
+  //T generico por defecto
+  async get<T>(url: string): Promise<T> {
+    const { data } = await this.axios.get<T>(url);
     return data;
   }
 
--- @nest-basic-stuff/@ts-basic-stuf/intro/src/bases/injection.ts
+++ @nest-basic-stuff/@ts-basic-stuf/intro/src/bases/injection.ts
@@ -1,8 +1,5 @@
-import type {
-  Move,
-  PokeapiResponse,
-} from "../interfaces/pokeapi-response.interface";
-import { PokeApiAdapter } from "../api/pokeApi.adapter";
+import type { Move, PokeapiResponse } from "../interfaces/pokeapi-response.interface";
+import { PokeApiAdapter, PokeApiFetchAdapter } from "../api/pokeApi.adapter";
 
 export class Pokemon {
   get imageUrl(): string {
@@ -15,7 +12,7 @@ export class Pokemon {
     // TODO: inyectar dependencias
 
     private readonly http: PokeApiAdapter,
-  ) {}
+  ) { }
 
   scream() {
     console.log(`${this.name.toUpperCase()}!!!`);
@@ -26,19 +23,24 @@ export class Pokemon {
   }
 
   async getMoves(): Promise<Move[]> {
-    const data = await this.http.get(
+    //un generico practicamente da a entender que tipo de dato vamos a trabajar. En este caso data va a ser de tipo Move[] (un arreglo de Moves)
+
+    const data = await this.http.get<PokeapiResponse>(
       `https://pokeapi.co/api/v2/pokemon/${this.id}`,
     );
+
     return data.moves;
   }
 }
 
+// Sustitución de Liskov: podemos sustituir una clase por otra, siempre y cuando ambas clases implementen la misma interfaz. En este caso, podemos sustituir PokeApiAdapter por PokeApiFetchAdapter, ya que ambas clases implementan el mismo método get<T>(url: string): Promise<T>.
 const pokeApi = new PokeApiAdapter();
+const pokeApiFetch = new PokeApiFetchAdapter();
 
 export const bulbasaur = new Pokemon(1, "Bulbasaur", pokeApi);
 bulbasaur.getMoves();
 
-export const charmander = new Pokemon(4, "Charmander", pokeApi);
+export const charmander = new Pokemon(4, "Charmander", pokeApiFetch);
 charmander.getMoves();
 
 console.log(bulbasaur);
